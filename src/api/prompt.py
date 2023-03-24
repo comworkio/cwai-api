e840a83933dbeba4e831abe0f73e63d111cb0db4
Idriss Neumann <idriss.neumann@comwork.io>
2023-03-24 14:02:54 +0100

    Fix: default values pydantic

--- src/api/prompt.py
+++ src/api/prompt.py
@@ -1,11 +1,12 @@
+from typing import Optional
 from main import app
 from pydantic import BaseModel
 from utils.gpt2 import generate_response, _default_max_length, _default_num_return_sequences
 
 class Prompt(BaseModel):
     message: str
-    max_length: int | _default_max_length
-    num_return_sequences: int | _default_num_return_sequences
+    max_length: Optional[int] = _default_max_length
+    num_return_sequences: Optional[int] = _default_num_return_sequences
 
 @app.post("/v1/prompt")
 def post_prompt(prompt: Prompt):
