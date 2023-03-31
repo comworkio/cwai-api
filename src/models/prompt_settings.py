1b53b9516b53f94cdad0b72c4c407036df48623c
Idriss Neumann <idriss.neumann@comwork.io>
2023-03-31 16:38:39 +0100

    Fix: remove commas

--- src/models/prompt_settings.py
+++ src/models/prompt_settings.py
@@ -7,9 +7,9 @@ class PromptSettings(BaseModel):
     max_length: Optional[int] = int(os.environ['DEFAULT_MAX_LENGTH'])
     num_return_sequences: Optional[int] = int(os.environ['DEFAULT_NUM_RETURN_SEQUENCES'])
     no_repeat_ngram_size: Optional[int] = int(os.environ['DEFAULT_NO_REPEAT_NGRAM_SIZE'])
-    early_stopping: Optional[bool] = True,
-    do_sample: Optional[bool] = True,
-    skip_special_tokens: Optional[bool] = True,
-    top_k: Optional[int] = int(os.environ['DEFAULT_TOP_K']),
-    top_p: Optional[float] = float(os.environ['DEFAULT_TOP_P']),
+    early_stopping: Optional[bool] = True
+    do_sample: Optional[bool] = True
+    skip_special_tokens: Optional[bool] = True
+    top_k: Optional[int] = int(os.environ['DEFAULT_TOP_K'])
+    top_p: Optional[float] = float(os.environ['DEFAULT_TOP_P'])
     temperature: Optional[float] = float(os.environ['DEFAULT_TEMPERATURE'])
