315be4e702bea2e8735e26a520778afb173383ca
Idriss Neumann <idriss.neumann@comwork.io>
2023-03-31 11:59:06 +0100

    Add a volume for cwai

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -10,5 +10,7 @@ env|grep -E "^(DEFAULT)_"|while read -r; do
   echo "${REPLY}" >> "${ENV_FILE}"
 done
 
+mkdir -p /ml-research/cwai/huggingface
+
 docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_REGISTRY_PASSWORD}"
 docker-compose -f "docker-compose-${ENV}.yml" up -d --force-recreate
