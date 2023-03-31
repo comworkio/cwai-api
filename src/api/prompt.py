610673cbcc40bb106ecbad4f2a4debb1866011b3
Idriss Neumann <idriss.neumann@comwork.io>
2023-03-31 11:01:36 +0100

    Fix: increment webservices version

--- src/api/prompt.py
+++ src/api/prompt.py
@@ -4,6 +4,7 @@ from typing import Optional
 from main import app
 
 from models.prompt import Prompt
+from models.prompt_settings import PromptSettings
 from models.v2_prompt import V2Prompt
 from utils.default_values import get_first_model
 
@@ -15,6 +16,7 @@ def post_prompt_v1(prompt: Prompt):
 def post_prompt_v2(prompt: V2Prompt, model: str):
     new_prompt = Prompt()
     new_prompt.message = prompt.message
+    new_prompt.settings = PromptSettings()
     return generate_prompt(new_prompt, model)
 
 @app.post("/v3/prompt/{model}")
