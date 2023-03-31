4c1345cc8a4c0fd12e685645eb32b1ccd8509789
Idriss Neumann <idriss.neumann@comwork.io>
2023-03-31 17:30:35 +0100

    Revert "Revert "Code duplication""
    This reverts commit 916cc3104d62ce2d6b286e2740c3ea37c06df92b.

--- src/drivers/bloom.py
+++ src/drivers/bloom.py
@@ -3,8 +3,8 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from drivers.model_driver import ModelDriver
 from models.prompt import Prompt
-from utils.default_values import get_max_length, get_no_repeat_ngram_size, get_num_return_sequences, get_temperature, get_top_k, get_top_p
 from utils.logger import log_msg
+from utils.transformers_utils import get_response
 
 _bloom_model_name = "bigscience/bloom"
 _bloom_model = AutoModelForCausalLM.from_pretrained(_bloom_model_name)
@@ -15,21 +15,4 @@ class BloomDriver(ModelDriver):
         log_msg("INFO", "[bloom] loading model...")
 
     def generate_response(self, prompt: Prompt):
-        input_ids = _bloom_tokenizer.encode(prompt.message, return_tensors="pt")
-        num_return_sequences=get_num_return_sequences(prompt)
-        output = _bloom_model.generate(input_ids, 
-            max_length=get_max_length(prompt), 
-            num_beams=5, 
-            no_repeat_ngram_size=get_no_repeat_ngram_size(prompt), 
-            num_return_sequences=num_return_sequences,
-            do_sample=prompt.settings.do_sample,
-            early_stopping=prompt.settings.early_stopping,
-            top_k=get_top_k(prompt),
-            top_p=get_top_p(prompt),
-            temperature=get_temperature(prompt),
-        )
-
-        response = []
-        for idx in range(num_return_sequences):
-            response.append(_bloom_model.decode(output[idx], skip_special_tokens=prompt.settings.skip_special_tokens))
-        return response
+        return get_response(prompt, _bloom_tokenizer, _bloom_model)
