708ee3bf198904d7e455f43db36839a437b71f9f
Idriss Neumann <idriss.neumann@comwork.io>
2023-05-26 16:26:28 +0100

    Fix: status code

--- src/api/prompt.py
+++ src/api/prompt.py
@@ -2,27 +2,34 @@ import importlib
 
 from main import app
 
+from fastapi import Response
 from models.prompt import Prompt
 from models.prompt_settings import PromptSettings
 from models.simple_prompt import SimplePrompt
-from utils.common import is_true
+from utils.common import is_false
 from utils.default_values import get_first_model
 from utils.logger import log_msg
 
-@app.post("/v1/prompt")
-def post_prompt_v1(prompt: SimplePrompt):
+@app.post("/v1/prompt", status_code = 200)
+def post_prompt_v1(prompt: SimplePrompt, response: Response):
     result = generate_prompt(convert_simple_to_extended(prompt), get_first_model())
-    return result, 200 if is_true(result['status']) else 400
+    if is_false(result['status']):
+        response.status_code = 400
+    return result
 
-@app.post("/v2/prompt/{model}")
-def post_prompt_v2(prompt: SimplePrompt, model: str):
+@app.post("/v2/prompt/{model}", status_code = 200)
+def post_prompt_v2(prompt: SimplePrompt, model: str, response: Response):
     result = generate_prompt(convert_simple_to_extended(prompt), model)
-    return result, 200 if is_true(result['status']) else 400
+    if is_false(result['status']):
+        response.status_code = 400
+    return result
 
-@app.post("/v3/prompt/{model}")
-def post_prompt_v3(prompt: Prompt, model: str):
+@app.post("/v3/prompt/{model}", status_code = 200)
+def post_prompt_v3(prompt: Prompt, model: str, response: Response):
     result = generate_prompt(prompt, model)
-    return result, 200 if is_true(result['status']) else 400
+    if is_false(result['status'], response: Response):
+        response.status_code = 400
+    return result
 
 def convert_simple_to_extended(prompt: SimplePrompt):
     return Prompt(message=prompt.message, settings=PromptSettings())
