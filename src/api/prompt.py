a453a2a882dbaddf9464fd25ee2838607465a7f7
Idriss Neumann <idriss.neumann@comwork.io>
2023-05-26 16:17:23 +0100

    Fix: key error

--- src/api/prompt.py
+++ src/api/prompt.py
@@ -12,17 +12,17 @@ from utils.logger import log_msg
 @app.post("/v1/prompt")
 def post_prompt_v1(prompt: SimplePrompt):
     result = generate_prompt(convert_simple_to_extended(prompt), get_first_model())
-    return result, 200 if is_true(result['ok']) else 400
+    return result, 200 if is_true(result['status']) else 400
 
 @app.post("/v2/prompt/{model}")
 def post_prompt_v2(prompt: SimplePrompt, model: str):
     result = generate_prompt(convert_simple_to_extended(prompt), model)
-    return result, 200 if is_true(result['ok']) else 400
+    return result, 200 if is_true(result['status']) else 400
 
 @app.post("/v3/prompt/{model}")
 def post_prompt_v3(prompt: Prompt, model: str):
     result = generate_prompt(prompt, model)
-    return result, 200 if is_true(result['ok']) else 400
+    return result, 200 if is_true(result['status']) else 400
 
 def convert_simple_to_extended(prompt: SimplePrompt):
     return Prompt(message=prompt.message, settings=PromptSettings())
